6fbfcf2aacb49bb5223a09e5aa9c44f0a55f0e7f
rswebmarketing <104783326+rswebmarketing@users.noreply.github.com>
2022-05-12 18:44:10 +0200

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -1,9 +1,5 @@
-<!-- Global site tag (gtag.js) - Google Ads: 10836610403 -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=AW-10836610403"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'AW-10836610403');
-</script>
