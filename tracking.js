22993de4b8fda2597e0233aae3ffb3cde36fb39b
rswebmarketing <104783326+rswebmarketing@users.noreply.github.com>
2022-05-19 19:04:56 +0200

    Update tracking.js

--- tracking.js
+++ tracking.js
@@ -2,9 +2,3 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10836610403"></script>
 <script>
 </script>
-{% if first_time_accessed %}
-<!-- Event snippet for Clic lien formulaire devis conversion page -->
-<script>
-  gtag('event', 'conversion', {'send_to': 'AW-10836610403/hhZsCM-rwLcDEOOapq8o'});
-</script>
-{% endif %}
